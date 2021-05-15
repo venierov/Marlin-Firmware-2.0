24862b967ec71ab80bd2744a5a07d0dc3b3c62d7
Veniero Vignoli <EVIGNOV5T@nttdata.com>
2021-05-15 03:08:34 +0200

    fix (ENDSTOP_NOISE_THRESHOLD)

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -710,8 +710,8 @@
 #endif
 
 // Mechanical endstop with COM to ground and NC to Signal uses "false" here (most common setup).
-#define X_MIN_ENDSTOP_INVERTING true // Set to true to invert the logic of the endstop.
-#define Y_MIN_ENDSTOP_INVERTING true // Set to true to invert the logic of the endstop.
+#define X_MIN_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
+#define Y_MIN_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
 #define Z_MIN_ENDSTOP_INVERTING true // Set to true to invert the logic of the endstop.
 //#define X_MAX_ENDSTOP_INVERTING true // Set to true to invert the logic of the endstop.
 //#define Y_MAX_ENDSTOP_INVERTING true // Set to true to invert the logic of the endstop.
@@ -769,7 +769,7 @@
  *
  * :[2,3,4,5,6,7]
  */
-#define ENDSTOP_NOISE_THRESHOLD 2
+//#define ENDSTOP_NOISE_THRESHOLD 2
 
 // Check for stuck or disconnected endstops during homing moves.
 //#define DETECT_BROKEN_ENDSTOP
